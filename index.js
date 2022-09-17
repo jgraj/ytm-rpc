8c652bdb47ff4c929c49fdd42235679857dd1048
Julian Grajewski <jgraj@jgraj.me>
2022-09-17 03:21:36 +0200

    Timeout, printing more info

--- index.js
+++ index.js
@@ -9,24 +9,40 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+let lastTitle;
 let lastTime;
+let timeout = 0;
 
 app.post('/update', (req, res) => {
 	const data = req.body;
 
-	console.log(data);
-
 	if (!data.title || !data.subtitle) {
-		console.error('Invalid song data received.')
+		console.error('Invalid song data received. 😕')
 		res.sendStatus(400);
 		return;
+	} else {
+		res.sendStatus(200);
 	}
 
 	if (!connected) {
 		return;
 	}
 
+	if (lastTitle != data.title) {
+		console.log(`🎶 Now playing "${data.title}".`);
+		timeout = 0;
+	}
+
 	const playing = lastTime != data.time;
+	if (!playing) {
+		if (++timeout > 60) {
+			console.log('😴 Was idle for 60 seconds, clearing activity.');
+			rpc.clearActivity();
+			return;
+		}
+	} else {
+		timeout = 0;
+	}
 
 	rpc.setActivity({
 		state: data.subtitle,
@@ -41,32 +57,44 @@ app.post('/update', (req, res) => {
 		}] : [],
 	});
 
+	lastTitle = data.title;
 	lastTime = data.time;
-
-	res.sendStatus(200);
 });
 
 let rpc;
 let connected = false;
+let retrying;
 
 const connectToDiscord = () => {
+	if (retrying) {
+		return;
+	}
+
 	rpc = new discord.Client({ transport: 'ipc' });
+	process.stdout.write('Connecting to Discord...');
 
 	rpc.login({ clientId: APP_ID }).then(() => {
 		connected = true;
-		console.log('Connected to Discord.');
+		console.log(' ✅.');
 	}).catch(() => {
-		console.log('Could not connect to Discord. Retrying in 5 seconds...');
-		setTimeout(connectToDiscord, 5000);
+		console.log(' ❎. Retrying in 5 seconds... ⌛');
+
+		retrying = setTimeout(() => {
+			retrying = false;
+			connectToDiscord();
+		}, 5000);
 	});
 
 	rpc.on('disconnected', () => {
 		connected = false;
-		console.log('Disconnected from Discord...');
+		console.log('Disconnected from Discord... 🤨');
+		
 		connectToDiscord();
 	});
 };
 
 process.stdout.write('Starting ytm-rpc...');
-app.listen(PORT, () => console.log(' ytm-rpc is ready.'));
-connectToDiscord();
+app.listen(PORT, () => {
+	console.log(' ✅.');
+	connectToDiscord();
+});
